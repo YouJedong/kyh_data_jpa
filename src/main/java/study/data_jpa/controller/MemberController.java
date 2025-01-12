18d70f24e43540803b7c9f7ec95aa3c3ac516b42
YouJedong <mraiou1@naver.com>
2025-01-12 21:10:28 +0900

    update: 강의 12일차 - 페이징 및 정렬

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,11 +2,16 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +21,9 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        Member member = new Member("member1");
-        memberRepository.save(member);
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
     @GetMapping("/members/{id}")
@@ -30,6 +36,13 @@ public class MemberController {
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
     }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<MemberDto> map = memberRepository.findAll(pageable).map(member -> new MemberDto(member.getId(),member.getUsername(), null));
+        return map;
+    }
+
 
 
 
